48d2058648a1f9205d7e111137041f6a20d7b1a2
pkq2006 <pkq2006@gmail.com>
2017-01-15 17:17:29 +0800

    cntk 95% and give up convolution

--- cntk/test.py
+++ cntk/test.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 import os
 import cntk
+import math
 import read_data as rd
 
 abs_path  = os.path.dirname(os.path.abspath(__file__))
@@ -10,7 +11,8 @@ data_path = os.path.join(abs_path, "data")
 
 #parameters
 my_strides = (2, 1)
-my_rf_shape = (3, 1)
+my_conv_rf_shape = (3, 1)
+my_pool_rf_shape = (2, 1)
 my_drop = 0.1
 learning_rate = 0.001
 input_height = 40
@@ -23,34 +25,26 @@ def net():
     input_var = cntk.ops.input_variable((num_channels, input_height, input_width), np.float32)
     label_var = cntk.ops.input_variable(num_output_classes, np.float32)
 
-    with cntk.layers.default_options():
-        '''
-        conv1 = cntk.layers.Convolution(my_rf_shape, activation = cntk.ops.relu, num_filters = 64, strides = my_strides, pad = True, bias = True, init = cntk.initializer.he_normal(), init_bias = cntk.initializer.he_normal())(input_var)
-        sys.stderr.write("conv1" + str(conv1.shape) + '\n')
-        pool1 = cntk.layers.MaxPooling(my_rf_shape, strides = my_strides, pad = True)(conv1)
-        sys.stderr.write("pool1" + str(pool1.shape) + '\n')
-        conv2 = cntk.layers.Convolution(my_rf_shape, activation = cntk.ops.relu, num_filters = 72, strides = my_strides, pad = True, bias = True, init = cntk.initializer.he_normal(), init_bias = cntk.initializer.he_normal())(pool1)
-        sys.stderr.write("conv2" + str(conv2.shape) + '\n')
-        pool2 = cntk.layers.MaxPooling(my_rf_shape, strides = my_strides, pad = True)(conv2)
-        sys.stderr.write("pool2" + str(pool2.shape) + '\n')
-        conv3 = cntk.layers.Convolution(my_rf_shape, activation = cntk.ops.relu, num_filters = 96, strides = my_strides, pad = True, bias = True, init = cntk.initializer.he_normal(), init_bias = cntk.initializer.he_normal())(pool2)
-        sys.stderr.write("conv3" + str(conv3.shape) + '\n')
-        pool3 = cntk.layers.MaxPooling((2, 1), strides = my_strides, pad = True)(conv3)
-        sys.stderr.write("pool3" + str(pool3.shape) + '\n')
-        '''
-        fc1   = cntk.layers.Dense(96, activation = cntk.ops.relu, bias = True, init = cntk.initializer.he_normal(), init_bias = cntk.initializer.he_normal())(input_var)
-        #sys.stderr.write("fc1" + str(fc1.shape) + '\n')
+    with cntk.layers.default_options(init = cntk.initializer.he_normal(), activation = cntk.ops.relu, bias = True, init_bias = cntk.initializer.he_normal()):
+        conv1 = cntk.layers.Convolution(my_conv_rf_shape, num_filters = 64, strides = my_strides, pad = True)(input_var)
+        pool1 = cntk.layers.MaxPooling(my_pool_rf_shape, strides = my_strides, pad = True)(conv1)
+
+        conv2 = cntk.layers.Convolution(my_conv_rf_shape, num_filters = 72, strides = my_strides, pad = True, bias = True)(pool1)
+        pool2 = cntk.layers.MaxPooling(my_pool_rf_shape, strides = my_strides, pad = True)(conv2)
+
+        conv3 = cntk.layers.Convolution(my_conv_rf_shape, num_filters = 96, strides = my_strides, pad = True)(pool2)
+        pool3 = cntk.layers.MaxPooling(my_pool_rf_shape, strides = my_strides, pad = True)(conv3)
+
+        fc1   = cntk.layers.Dense(96)(input_var)
         drop1 = cntk.layers.Dropout(my_drop)(fc1)
-        #sys.stderr.write("drop1" + str(drop1.shape) + '\n')
-        fc2   = cntk.layers.Dense(72, activation = cntk.ops.relu, bias = True, init = cntk.initializer.he_normal(), init_bias = cntk.initializer.he_normal())(drop1)
-        #sys.stderr.write("fc2" + str(fc2.shape) + '\n')
+
+        fc2   = cntk.layers.Dense(72)(drop1)
         drop2 = cntk.layers.Dropout(my_drop)(fc2)
-        #sys.stderr.write("drop2" + str(drop2.shape) + '\n')
-        fc3   = cntk.layers.Dense(64, activation = cntk.ops.relu, bias = True, init = cntk.initializer.he_normal(), init_bias = cntk.initializer.he_normal())(drop2)
-        #sys.stderr.write("fc3" + str(fc3.shape) + '\n')
+
+        fc3   = cntk.layers.Dense(64)(drop2)
         drop3 = cntk.layers.Dropout(my_drop)(fc3)
-        #sys.stderr.write("drop3" + str(drop3.shape) + '\n')
-        out   = cntk.layers.Dense(num_output_classes)(drop3)
+
+        out   = cntk.layers.Dense(num_output_classes, activation = None)(drop3)
 
     ce = cntk.ops.cross_entropy_with_softmax(out, label_var)
     pe = cntk.ops.classification_error(out, label_var)
@@ -60,7 +54,7 @@ def net():
 
     lr_per_sample    = [learning_rate]
     lr_schedule      = cntk.learning_rate_schedule(lr_per_sample, cntk.learner.UnitType.sample, epoch_size)
-    mm_time_constant = [0]
+    mm_time_constant = [1100]
     mm_schedule      = cntk.learner.momentum_as_time_constant_schedule(mm_time_constant, epoch_size)
 
     learner = cntk.learner.adam_sgd(out.parameters, lr = lr_schedule, momentum = mm_schedule)
