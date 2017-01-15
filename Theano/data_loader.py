eb6f562d77d78edf8022f39fe2db18c33df57720
niconiconi <niconiconi@sjtu.edu.cn>
2017-01-16 00:51:31 +0800

    Update data_loader.py

--- Theano/data_loader.py
+++ Theano/data_loader.py
@@ -1,7 +1,7 @@
 import read_data_fast as rd
-def load_data(iter_num, batch):
+def load_data():
 	rd.read_data_()
 	datasets = [[], []]
-	datasets[0] = rd.read_training_batch(int((iter_num + batch) / batch), batch)
+	datasets[0] = rd.read_training_batch()
 	datasets[1] = rd.read_test()
 	return datasets
